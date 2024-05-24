55e2c6edb3141bf4ca80f37773f3ecf1f4616171
Galih Prayoga <galihp83@gmail.com>
2024-05-24 13:49:08 +0700

    database main

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"olshop/config"
+	"olshop/utilities/database"
+)
+
+func main() {
+	var dbConfig = new(config.DatabaseMysql)
+	if err := dbConfig.LoadFromEnv(); err != nil {
+		panic(err)
+	}
+
+	dbConnection, err := database.MysqlInit(*dbConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := database.MysqlMigrate(dbConnection); err != nil {
+		panic(err)
+	}
+
+}
