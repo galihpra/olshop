e15a9214d58773dbae630580b9c358332a8ade81
Galih Prayoga <galihp83@gmail.com>
2024-05-30 14:30:50 +0700

    cloudinary utilities

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.21.1
 require golang.org/x/crypto v0.23.0
 
 require (
+	github.com/cloudinary/cloudinary-go/v2 v2.7.0 // indirect
+	github.com/creasty/defaults v1.5.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
---
+++ utilities/cloudinary/cloudinary.go
@@ -0,0 +1,45 @@
+package cloudinary
+
+import (
+	"context"
+	"io"
+	"olshop/config"
+
+	cld "github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+)
+
+type Cloud interface {
+	Upload(ctx context.Context, folder string, Raw io.Reader) (*string, error)
+}
+
+func NewCloudinary(config config.Cloudinary) (Cloud, error) {
+	client, err := cld.NewFromParams(config.CloudName, config.ApiKey, config.ApiSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cloudinary{
+		config: config,
+		client: client,
+	}, nil
+}
+
+type cloudinary struct {
+	config config.Cloudinary
+	client *cld.Cloudinary
+}
+
+func (cloud *cloudinary) Upload(ctx context.Context, folder string, Raw io.Reader) (*string, error) {
+	UniqueFilename := true
+	res, err := cloud.client.Upload.Upload(ctx, Raw, uploader.UploadParams{
+		UniqueFilename: &UniqueFilename,
+		Folder:         folder,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &res.SecureURL, nil
+}
