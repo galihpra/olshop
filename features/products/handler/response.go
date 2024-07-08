e3daa26e018fe92fa4abdf5aeebae7e6f9bcc468
Galih Prayoga <galihp83@gmail.com>
2024-07-08 14:13:14 +0700

    add preoduct response

--- features/products/handler/response.go
+++ features/products/handler/response.go
@@ -1,1 +1,13 @@
 package handler
+
+type ProductResponse struct {
+	Id          uint    `json:"product_id,omitempty"`
+	Name        string  `json:"name,omitempty"`
+	Description string  `json:"description,omitempty"`
+	Price       float64 `json:"price,omitempty"`
+	Discount    int     `json:"discount,omitempty"`
+	Rating      float32 `json:"rating,omitempty"`
+
+	Thumbnail string   `json:"thumbnail,omitempty"`
+	Images    []string `json:"images,omitempty"`
+}
