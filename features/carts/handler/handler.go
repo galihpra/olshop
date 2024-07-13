be0c3f795ed4f29c304eaccb3f55183383b11769
Galih Prayoga <galihp83@gmail.com>
2024-07-13 14:56:57 +0700

    create cart handler

---
+++ features/carts/handler/handler.go
@@ -0,0 +1,90 @@
+package handler
+
+import (
+	"net/http"
+	"olshop/config"
+	"olshop/features/carts"
+	tokens "olshop/helpers/token"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+type cartHandler struct {
+	service   carts.Service
+	jwtConfig config.JWT
+}
+
+func NewCartHandler(service carts.Service, jwtConfig config.JWT) carts.Handler {
+	return &cartHandler{
+		service:   service,
+		jwtConfig: jwtConfig,
+	}
+}
+
+func (handler *cartHandler) Create() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var request = new(CartRequest)
+		var response = make(map[string]any)
+
+		token := c.Get("user")
+		if token == nil {
+			response["message"] = "unauthorized access"
+			return c.JSON(http.StatusUnauthorized, response)
+		}
+
+		userId, err := tokens.ExtractToken(handler.jwtConfig.Secret, token.(*jwt.Token))
+		if err != nil {
+			c.Logger().Error(err)
+
+			response["message"] = "unauthorized"
+			return c.JSON(http.StatusUnauthorized, response)
+		}
+
+		if err := c.Bind(request); err != nil {
+			c.Logger().Error(err)
+
+			response["message"] = "incorect input data"
+			return c.JSON(http.StatusBadRequest, response)
+		}
+
+		var parseInput = new(carts.Cart)
+		parseInput.ProductID = request.ProductId
+		parseInput.VarianID = request.VarianId
+		parseInput.Quantity = request.Quantity
+		parseInput.UserID = userId
+
+		if err := handler.service.Create(c.Request().Context(), *parseInput, uint(userId)); err != nil {
+			c.Logger().Error(err)
+
+			if strings.Contains(err.Error(), "validate") {
+				response["message"] = strings.ReplaceAll(err.Error(), "validate: ", "")
+				return c.JSON(http.StatusBadRequest, response)
+			}
+
+			if strings.Contains(err.Error(), "unauthorized") {
+				response["message"] = "unauthorized"
+				return c.JSON(http.StatusBadRequest, response)
+			}
+
+			response["message"] = "internal server error"
+			return c.JSON(http.StatusInternalServerError, response)
+		}
+
+		response["message"] = "product added to cart"
+		return c.JSON(http.StatusCreated, response)
+	}
+}
+
+func (handler *cartHandler) Delete() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+func (handler *cartHandler) GetAll() echo.HandlerFunc {
+	panic("unimplemented")
+}
+
+func (handler *cartHandler) Update() echo.HandlerFunc {
+	panic("unimplemented")
+}
