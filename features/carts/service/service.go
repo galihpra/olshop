a1c856892cf9ad95cad102dad502e03cd99ec116
Galih Prayoga <galihp83@gmail.com>
2024-07-13 14:56:42 +0700

    create cart service

---
+++ features/carts/service/service.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"olshop/features/carts"
+)
+
+type cartService struct {
+	repo carts.Repository
+}
+
+func NewCartService(repo carts.Repository) carts.Service {
+	return &cartService{
+		repo: repo,
+	}
+}
+
+func (service *cartService) Create(ctx context.Context, newCart carts.Cart, userId uint) error {
+	if newCart.ProductID == 0 {
+		return errors.New("validate: product can't be empty")
+	}
+	if newCart.Quantity == 0 {
+		return errors.New("validate: quantity can't be empty")
+	}
+
+	if err := service.repo.Create(ctx, newCart, userId); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (service *cartService) Delete(ctx context.Context, cartId uint, userId uint) error {
+	panic("unimplemented")
+}
+
+func (service *cartService) GetAll(ctx context.Context, userId uint) ([]carts.Cart, error) {
+	panic("unimplemented")
+}
+
+func (service *cartService) Update(ctx context.Context, cartId uint, userId uint, updateCart carts.Cart) error {
+	panic("unimplemented")
+}
