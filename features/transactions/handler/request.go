74b6c0c5a66489234181c76b86ff5f5616856c00
Galih Prayoga <galihp83@gmail.com>
2024-07-18 23:10:08 +0700

    add transaction request

--- features/transactions/handler/request.go
+++ features/transactions/handler/request.go
@@ -1,1 +1,11 @@
 package handler
+
+type CartItem struct {
+	CartID uint `json:"cart_id"`
+}
+
+type TransactionRequest struct {
+	AddressId     uint       `json:"address_id"`
+	PaymentMethod string     `json:"payment_method"`
+	Items         []CartItem `json:"items"`
+}
