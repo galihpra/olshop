5b49827c282995bc08f719bb035fcc714ed26cf4
Galih Prayoga <galihp83@gmail.com>
2024-09-03 14:12:31 +0700

    change service

--- features/transactions/service/service.go
+++ features/transactions/service/service.go
@@ -16,18 +16,20 @@ func NewTransactionService(repo transactions.Repository) transactions.Service {
 	}
 }
 
-func (service *transactionService) Create(ctx context.Context, userId uint, cartIds []uint, newTransaction transactions.Transaction) error {
+func (service *transactionService) Create(ctx context.Context, userId uint, cartIds []uint, newTransaction transactions.Transaction) (*transactions.Transaction, error) {
 	if newTransaction.PaymentMethod == "" {
-		return errors.New("validate: payment method can't be empty")
+		return nil, errors.New("validate: payment method can't be empty")
 	}
 
 	if len(cartIds) == 0 {
-		return errors.New("validate: no cart selected")
+		return nil, errors.New("validate: no cart selected")
 	}
 
-	if err := service.repo.Create(ctx, userId, cartIds, newTransaction); err != nil {
-		return err
+	result, err := service.repo.Create(ctx, userId, cartIds, newTransaction)
+
+	if err != nil {
+		return nil, err
 	}
 
-	return nil
+	return result, nil
 }
